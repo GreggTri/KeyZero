83c361a5dd56f9b3f6b20e2a2c8ac07257c4bf7e
Gregg Trimarchi <gttrimarchig@gmail.com>
2023-06-26 20:04:12 -0700

    Search Page is responsive enough. still some issues but not a big deal

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -10,26 +10,34 @@ export default function Search() {
     return (
         <div className='flex flex-col items-center h-screen p-3 bg-background'>
             <h1 className="text-2xl my-14">Ask a question against your companies documentation</h1>
-            <form method="post" action="" className="w-2/6 shadow-md">
-                <div className="relative w-full text-gray-600 focus-within:text-gray-400">
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                        <img src="assets/searchIcon.svg" alt="SearchIcon" className="w-full h-4" />
-                    </span>
-                    <input type="search" name="q" className="w-full py-2 pl-8 text-sm text-white rounded-md bg-secondary focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Ask a Question..." required/>
+            {/* <div className="flex flex-row w-full px-1 bg-white rounded-md shadow-md">
+                <form method="post" action="" className="flex flex-row items-center w-full">
+                    <img src="assets/searchIcon.svg" alt="SearchIcon" className="w-full h-4" />
+                    <input type="search" className="py-2 pl-1 mr-2 text-sm text-white focus:outline-none focus:text-gray-900" placeholder="Ask a Question..." required/>
+                </form>
+            </div> */}
+            
+            <form className="flex items-center w-3/4 md:w-1/2 lg:w-1/3">   
+                <div className="relative w-full">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" className="w-5 h-5 text-black dark:text-black" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path></svg>
+                    </div>
+                    <input type="search" className="bg-white text-gray-900 text-sm rounded-md shadow-md focus:outline-none block w-full pl-10 p-2.5" placeholder="Ask a Question..." required/>
                 </div>
             </form>
+
             {
-            true ?
-            <div className="flex flex-col w-full bg-white rounded-md shadow-md my-14">
+            response ?
+            <div className="flex flex-col w-full bg-white rounded-md shadow-md md:w-4/6 my-14">
                 <div className="flex flex-row justify-between">
-                    <h2 className="p-4 text-md">Answer</h2>
+                    <h2 className="p-4 font-medium text-md">Answer</h2>
                     <div className="p-4 text-sm">
                         <span className="px-2 py-1 mx-2 text-green-600 bg-green-200 rounded">Good</span>
                         <span className="px-2 py-1 mx-2 text-red-600 bg-red-200 rounded">Bad</span>
                     </div>
                 </div>
-                <p className="p-6 text-sm">
-                test
+                <p className="px-6 py-4 text-sm">
+                
                 </p>
             </div>
             :
